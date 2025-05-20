a36b87d6b84e131a1a978ccdb91068fd2265a655
xyryc <amdanikzz@gmail.com>
2025-05-20 07:13:32 +0600

    fetched and rendered movies

--- services/api.ts
+++ services/api.ts
@@ -3,14 +3,14 @@ export const TMDB_CONFIG = {
   API_KEY: process.env.EXPO_PUBLIC_MOVIE_API_KEY,
   headers: {
     accept: "application/json",
-    Authentication: `Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_KEY}`,
+    Authorization: `Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_KEY}`,
   },
 };
 
 export const fetchMovies = async ({ query }: { query: string }) => {
   const endpoint = query
-    ? `${TMDB_CONFIG}/search/movie?query=${encodeURIComponent(query)}`
-    : `${TMDB_CONFIG}/discover/movie?sort_by=popularity.desc`;
+    ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+    : `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
 
   const response = await fetch(endpoint, {
     method: "GET",
@@ -25,5 +25,3 @@ export const fetchMovies = async ({ query }: { query: string }) => {
   const data = await response.json();
   return data.results;
 };
-
-// /discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
